4e77b46d3284161557c4408231020bcff00ce750
Henrique Baron <henrique.baron@gmail.com>
2021-11-18 21:19:05 +0100

    Adicionar o projeto do front-end em Angular
    Já com o componente para a lista de serviços, sem implementação.

---
+++ ui/angular13/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServicosComponent } from './servicos/servicos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServicosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
