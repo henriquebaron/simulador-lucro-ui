df21565e49cebf691e4904e436236dca1fa45931
Henrique Baron <henrique.baron@gmail.com>
2021-11-19 20:36:33 +0100

    Criar a tela inicial da página

--- ui/angular13/src/app/app-routing.module.ts
+++ ui/angular13/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListServicosComponent } from './list-servicos/list-servicos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'servicos', component:ListServicosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
