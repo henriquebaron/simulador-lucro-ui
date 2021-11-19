1e3286172bab3244edc20eb996bfcf84671a25af
Henrique Baron <henrique.baron@gmail.com>
2021-11-19 20:14:54 +0100

    Adicionar classe de serviço
    Para chamadas à API em C#.

--- ui/angular13/src/app/apicall.service.ts
+++ ui/angular13/src/app/apicall.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApicallService {
+  private readonly apiUrl = "http://localhost:5000/api";
 
-  constructor() { }
+  constructor(private api:HttpClient) { }
+
+  getListaServicos():Observable<any[]>{
+    return this.api.get<any>(this.apiUrl + "/servicos");
+  }
+
+  adicionarServico(servico:any){
+    return this.api.post(this.apiUrl + "/servicos", servico);
+  }
+
+  atualizarServico(servico:any){
+    return this.api.put(this.apiUrl + "/servicos/" + servico.id, servico);
+  }
+
+  removerServico(id:any){
+    return this.api.delete(this.apiUrl + "/servicos/" + id);
+  }
 }
