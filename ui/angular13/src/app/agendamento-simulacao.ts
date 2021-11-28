da65b5c6ad915aba5499945899cdabff08510cb6
Henrique Baron <henrique.baron@gmail.com>
2021-11-28 22:12:35 +0100

    Adicionar classe para agendamentos na simulação
    No front-end em TypeScript.

---
+++ ui/angular13/src/app/agendamento-simulacao.ts
@@ -0,0 +1,33 @@
+import { Servico } from "./servico";
+
+export class AgendamentoSimulacao {
+    hora: string = "00:00";
+    idServico: number = 0;
+
+    static getMinutos(agendamento: AgendamentoSimulacao): number {
+        return Number.parseInt(agendamento.hora.split(':')[1]);
+    }
+
+    static getHoras(agendamento: AgendamentoSimulacao): number {
+        return Number.parseInt(agendamento.hora.split(':')[0]);
+    }
+
+    static horaAsString(horas: number, minutos: number): string {
+        return horas.toString().padStart(2, '0') + ':' + minutos.toString().padStart(2, '0');
+    }
+
+    getHoraTermino(servico: Servico): string {
+        if (servico.id != this.idServico) throw new Error("O ID do parâmetro Serviço não corresponde ao ID de serviço do objeto atual.");
+
+        var minutos = AgendamentoSimulacao.getMinutos(this);
+        var horas = AgendamentoSimulacao.getHoras(this);
+
+        minutos += Servico.getMinutos(servico);
+        horas += Math.floor(minutos / 60);
+
+        minutos = minutos % 60;
+        horas += Servico.getHoras(servico);
+
+        return AgendamentoSimulacao.horaAsString(horas, minutos);
+    }
+}
